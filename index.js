3818a70bb8e02cbd44341ad8af5855e5ae273c28
bredele <olivier.wietrich@gmail.com>
2018-04-04 14:38:56 -0600

    Supports multiple destructuration

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 
 
 
-module.exports = (arr, key = 'name') => {
+/**
+ * Destructure an array into an object.
+ *
+ * @param {Array} arr
+ * @param {String?} key
+ * @param {String?} value
+ * @return {Object}
+ * @api public
+ */
+
+module.exports = (arr, key = 'name', value) => {
   const obj = {}
   arr.map(item => {
     const {
       [key] : name,
       ...props
     } = item
-    obj[name] = props
+    obj[name] = value ? props[value] : props
   })
   return obj
 }
